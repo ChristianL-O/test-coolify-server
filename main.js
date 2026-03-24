a49cb0f6c87c2d56308c5164de5892a1be3c1823
ChristianL-O <christianleono23@gmail.com>
2026-03-24 13:28:54 +0100

    twst

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 3002;
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello world" });
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
