2c39fdd0028b71a8fd159f7541a854a07c6dc7ff
ChristianL-O <christianleono23@gmail.com>
2026-03-25 08:47:18 +0100

    Endret port

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = 3000;
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello world" });
